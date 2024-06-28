acbc02db24decd81e2bd32d4051b6963bd882de1
Senne Pieters <2062262pieters@zuyd.nl>
2024-06-28 15:59:45 +0200

    test

--- FlirOneSensorInterface.py
+++ FlirOneSensorInterface.py
@@ -34,7 +34,7 @@ def calculate_max_temperature(temperature_values):
 
 # Function to write data to InfluxDB
 def write_to_influxdb(max_temperature, patient_id, current_time):
-    influxdb_url = "http://localhost:8086"
+    influxdb_url = "http://192.168.0.206:8086/"
     token = "MWvTgNMvClxm4CkZuUTvp4VHYHE_ifm3RCvbu45Gg3ZQjS33mBp_RCCcbf0bBBkVrwsFs5m_r1nz6G6Gmc5ojQ=="
     org = "LIME"
     bucket = "flironesensor"
